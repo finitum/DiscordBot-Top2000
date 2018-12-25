73d3a20d8eb980e65f1b437e91b8eb7df0668367
Victor <victor@xirion.net>
2018-12-25 20:32:23 +0100

    try info (not working)

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
+import time
 import api
 from discord.ext import commands
+from datetime import datetime
 
 # Creates the bot and specifies the prefix
 bot = commands.Bot(command_prefix="!")
 players = {}
+channels = []
 
 
 @bot.event
@@ -22,7 +25,9 @@ async def play(ctx):
     player = await voice_client.create_ytdl_player("http://icecast.omroep.nl/radio2-bb-mp3")
     player.start()
     players[server.id] = player
+    channels.append(ctx.message.channel)
     print("Playing")
+    await schedule_info()
 
 
 @bot.command(pass_context=True)
@@ -31,8 +36,7 @@ async def pause(ctx):
     players[server.id].stop()
 
 
-@bot.command(pass_context=True)
-async def song(ctx):
+def generate_current_song_embed():
     data = api.get_now_on_air()["songfile"]
 
     embed = discord.Embed(
@@ -44,7 +48,25 @@ async def song(ctx):
 
     embed.set_thumbnail(url=img)
 
-    await bot.say(embed=embed)
+    return embed
 
-# actually run the bot
-bot.run("NTI3MTE1MTk1MDU5OTI5MTE4.DwPPPA.9yeoo_IGMzMVAwBLfATt6LdVCaw")  # Needs to be moved to a file
+
+@bot.command()
+async def song():
+    await bot.say(embed=generate_current_song_embed())
+
+
+async def schedule_info():
+    for c in channels:
+        await bot.send_message(c, embed=generate_current_song_embed())
+
+    end = datetime.strptime(api.get_now_on_air()['stopdatetime'], "%Y-%m-%dT%H:%M:%S+01:00")
+    now = datetime.now()
+    run_at = end - now
+    delay = run_at.total_seconds()
+    time.sleep(delay)
+    await schedule_info()
+
+
+if __name__ == "__main__":
+    bot.run("NTI3MTE1MTk1MDU5OTI5MTE4.DwPPPA.9yeoo_IGMzMVAwBLfATt6LdVCaw")  # Needs to be moved to a file
